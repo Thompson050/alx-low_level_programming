76c16daa470c2e7a6f3ae0bd66553a2772e7e3e7
Thompson050 <annipaul200@gmail.com>
2022-07-07 06:50:12 -0700

    2nd commit

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* largest_number - returns the largest of 3 numbers
+* @a: first int
+* @b: second int
+* @c: third int
+* Return: largest number
+*/
+
+int largest_number(int a, int b, int c)
+{
+int largest = b;
+if (a >= b && a >= c)
+{
+largest = a;
+}
+else if (c >= b && c >=a)
+{
+largest = c;
+}
+return (largest);
+
+}
--- 0x03-debugging/README.md
+++ 0x03-debugging/README.md
@@ -1,1 +1,1 @@
-a file that comment out the part of the code that is causing the output to go into an infinite loop
+a file that fix the code so that it correctly prints out the largest of three numbers, no matter the case
